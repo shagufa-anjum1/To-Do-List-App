f799d593b754ee454e5815a4fff7376bb42de1a4
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2024-09-17 22:03:04 +0530

    Commit

--- script.js
+++ script.js
@@ -13,4 +13,27 @@ function addTask() {
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
+    saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
